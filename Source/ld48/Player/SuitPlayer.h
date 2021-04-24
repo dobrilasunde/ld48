d7709981b45b83bbe7bf7f09725f65e1cf7056db
DESKTOP-RG8MAIT\dobri <dobrila.sunde@gmail.com>
2021-04-24 16:26:51 +0200

    Added player base classes and Paper2D

---
+++ Source/ld48/Player/SuitPlayer.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+/*----------------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "../Plugins/2D/Paper2D/Source/Paper2D/Classes/PaperCharacter.h"
+#include "SuitPlayer.generated.h"
+/*----------------------------------------------------------------------------------------------------*/
+UCLASS()
+class LD48_API ASuitPlayer : public APaperCharacter
+{
+	GENERATED_BODY()
+
+
+};
+/*----------------------------------------------------------------------------------------------------*/
---
+++ Source/ld48/Player/SuitPlayerController.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+/*----------------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "SuitPlayerController.generated.h"
+/*----------------------------------------------------------------------------------------------------*/
+UCLASS()
+class LD48_API ASuitPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
+/*----------------------------------------------------------------------------------------------------*/
