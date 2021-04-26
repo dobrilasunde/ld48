8d3297f1c0069d9072d912b268bae5e98fcaf9d7
DESKTOP-RG8MAIT\dobri <dobrila.sunde@gmail.com>
2021-04-26 18:50:15 +0200

    Boss npc base class

---
+++ Source/ld48/NPC/BossNPC.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+/*----------------------------------------------------------------------------------------------------*/
+#include "BossNPC.h"
+#include "../Player/SuitPlayer.h"
+#include "../Player/SuitPlayerController.h"
+#include "Components/CapsuleComponent.h"
+#include "../Player/Projectile.h"
+/*----------------------------------------------------------------------------------------------------*/
+ABossNPC::ABossNPC(): Super()
+{
+
+}
+/*----------------------------------------------------------------------------------------------------*/
+/*override*/
+void ABossNPC::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+/*----------------------------------------------------------------------------------------------------*/
+/*override*/
+void ABossNPC::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GetCapsuleComponent())
+	{
+		GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &ABossNPC::OnOverlapBegin);
+	}
+}
+/*----------------------------------------------------------------------------------------------------*/
+void ABossNPC::MoveToTarget(AActor* target)
+{
+	ASuitPlayer* player = Cast<ASuitPlayer>(target);
+	if (player == nullptr)
+	{
+		return;
+	}
+
+	ASuitPlayerController* playerController = Cast<ASuitPlayerController>(player->GetController());
+	if (playerController == nullptr)
+	{
+		return;
+	}
+
+	TurnTowardsTarget(target);
+
+	Super::MoveToTarget(target);
+}
+/*----------------------------------------------------------------------------------------------------*/
+void ABossNPC::OnArrivedToTarget(AActor* target)
+{
+	Super::OnArrivedToTarget(target);
+}
+/*----------------------------------------------------------------------------------------------------*/
+void ABossNPC::AttackTarget(AActor* target)
+{
+	Super::AttackTarget(target);
+}
+/*----------------------------------------------------------------------------------------------------*/
+void ABossNPC::OnOverlapBegin(UPrimitiveComponent* overlappedComp, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult)
+{
+	if (AProjectile* projectile = Cast<AProjectile>(otherActor))
+	{
+		//Destroy();
+	}
+}
+/*----------------------------------------------------------------------------------------------------*/
---
+++ Source/ld48/NPC/BossNPC.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+/*----------------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "NPC.h"
+#include "BossNPC.generated.h"
+/*----------------------------------------------------------------------------------------------------*/
+UCLASS()
+class LD48_API ABossNPC : public ANPC
+{
+	GENERATED_BODY()
+
+public:
+	ABossNPC();
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void BeginPlay() override;
+
+	virtual void MoveToTarget(AActor* target) override;
+	virtual void OnArrivedToTarget(AActor* target) override;
+	virtual void AttackTarget(AActor* target) override;
+
+private:
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* overlappedComp, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult);
+};
+/*----------------------------------------------------------------------------------------------------*/
