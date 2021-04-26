058e3ad0bf8c73a532367d33ecf2419f618e1a7a
DESKTOP-RG8MAIT\dobri <dobrila.sunde@gmail.com>
2021-04-26 23:08:59 +0200

    Added final level

--- Source/ld48/Levels/LevelManager.cpp
+++ Source/ld48/Levels/LevelManager.cpp
@@ -20,6 +20,7 @@ void ALevelManager::BeginPlay()
 	_levelsLocked.Emplace(ELevelName::LevelGarage, false);
 	_levelsLocked.Emplace(ELevelName::LevelStairs, true);
 	_levelsLocked.Emplace(ELevelName::LevelOfficeElevator, false);
+	_levelsLocked.Emplace(ELevelName::FinalLevel, true);
 
 	SetCurrentLevel(_initialLevel);
 }
@@ -76,7 +77,29 @@ void ALevelManager::ResetLevels()
 /*----------------------------------------------------------------------------------------------------*/
 ALevelPrototype* ALevelManager::LoadLevel(const ELevelName& level)
 {
-	if (TSubclassOf<ALevelPrototype>* levelPtr = _levels.Find(level))
+	// Final level
+	if (_remainingEnemies == 0 && level == ELevelName::LevelGarage)
+	{
+		if (TSubclassOf<ALevelPrototype>* levelPtr = _levels.Find(ELevelName::FinalLevel))
+		{
+			TSubclassOf<ALevelPrototype> levelPrototypeClass = *levelPtr;
+
+			UWorld* world = GetWorld();
+			if (world == nullptr)
+			{
+				return nullptr;
+			}
+
+			UObject* CDO = levelPrototypeClass->GetDefaultObject();
+			if (CDO == nullptr)
+			{
+				return nullptr;
+			}
+
+			return world->SpawnActor<ALevelPrototype>(CDO->GetClass(), FVector::ZeroVector, FQuat::Identity.Rotator());
+		}
+	}
+	else if (TSubclassOf<ALevelPrototype>* levelPtr = _levels.Find(level))
 	{
 		TSubclassOf<ALevelPrototype> levelPrototypeClass = *levelPtr;
 
